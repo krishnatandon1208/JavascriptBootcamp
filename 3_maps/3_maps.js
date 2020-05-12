a74bcf70e58e97a6bfbdd0122e5ade30e6c26600
krishnatandon1208 <krishnatandon.1208@gmail.com>
2020-05-12 18:25:56 +0530

    map method

--- 3_maps/3_maps.js
+++
@@ -1,9 +0,0 @@
-//From the given array of strings, return another array such that all the string char are separated by dots.
-
-const stringList = ["asap", "ttly", "omg", "rofl"];
-
-const newStringList = stringList.map(function(s) {
-    return s.toUpperCase().split("").join(".");
-});
-
-console.log(newStringList);
